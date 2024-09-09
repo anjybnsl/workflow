9971099b1b90ae3b0a4e9044de1dc8801a775f79
anjybnsl <128787867+anjybnsl@users.noreply.github.com>
2024-09-09 20:06:17 +0530

    Foreground.jsx

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -10,31 +10,31 @@ function Foreground() {
     {
       desc: "Here is a new assignment for you. Please download it.",
       filesize: ".10mb",
-      close: false,
+      close: true,
       tag: {isOpen: true, tagTitle: "Download Now", tagColor: "sky"}
     },
     {
       desc: "Here are those files which are working in progress .",
       filesize: ".20mb",
-      close: true,
+      close: false,
       tag: {isOpen: false, tagTitle: "Download Now", tagColor: "blue"}
     },
     {
       desc: "Here, you can share your files with your team members.",
       filesize: ".40mb",
-      close: true,
+      close: false,
       tag: {isOpen: true, tagTitle: "Share", tagColor: "pink"}
     },
     {
       desc: "If an assignment is done. Kindly submit here.",
       filesize: ".50mb",
-      close: true,
+      close: false,
       tag: {isOpen: true, tagTitle: "Submit", tagColor: "green"}
     },
     {
       desc: "If you are getting any problem then reach out here.",
       filesize: "",
-      close: true,
+      close: false,
       tag: {isOpen: true, tagTitle: "Help", tagColor: "red"}
     }
   ];
